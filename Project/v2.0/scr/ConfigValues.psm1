668a6873473c4aff5362aa164399cb3b9978b9fe
Auros-NotePC <92504306+Auros-engineerVersion@users.noreply.github.com>
2022-11-08 13:51:32 +0900

    Change Property

--- Project/v2.0/scr/ConfigValues.psm1
+++ Project/v2.0/scr/ConfigValues.psm1
@@ -1,17 +1,30 @@
 class ConfigValues {
+    [string]$private:configName
+    [string]ConfigName() { return $this.configName }
+
     [string]$private:targetDirectoryPath
     [string]TargetDirectoryPath() { return $this.targetDirectoryPath }
 
     [string]$private:passwordsFilePath
     [string]PasswordsFilePath() { return $this.passwordsFilePath }
 
-    [string]$private:settingFilePath
-    [string]SettingFilePath() { return $this.settingFilePath }
+    [string]$private:destination
+    [string]Dest() { return $this.destination }
 
-    ConfigValues($targetDirectoryPath, $passwordsFilePath, $settingFilePath) {
+    ConfigValues($configName, $targetDirectoryPath, $passwordsFilePath, $dest) {
+        $this.configName = $configName
         $this.targetDirectoryPath = $targetDirectoryPath
         $this.passwordsFilePath = $passwordsFilePath
-        $this.settingFilePath = $settingFilePath
+        $this.destination = $dest
+    }
+
+    static [ConfigValues]CreateTemplateConfig() {
+        $name = "ConfigName:TemplateConfig"
+        $dirPath = "DirectoryPath:no data"
+        $passPath = "PasswordsPath:no data"
+        $dest = "Dest:no data"
+        $result = [ConfigValues]::new($name, $dirPath, $passPath, $dest)
+        return $result
     }
 
     [string[]]ToStrings() {
@@ -24,7 +37,7 @@ class ConfigValues {
         return $result
     }
 
-    [PSCustomObject]FromJson($jsonValue) {
+    [ConfigValues]FromJson($jsonValue) {
         $result = $jsonValue | ConvertFrom-Json
         return $result
     }
